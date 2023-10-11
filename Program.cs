b5a325681a2617f99a98c0e23da597aaaa98d637
Jeussu <94780400+Jeussu@users.noreply.github.com>
2023-10-11 09:50:00 +0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualBasic;
+using System;
+
+namespace LoopsIntro
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int counter = -3; counter < 4; counter++)
+            {
+                Console.WriteLine(counter);
+            }
+            Console.WriteLine("For loop is done");
+            Console.Read();            
+        }
+    }
+}
+
+
+
+
+
